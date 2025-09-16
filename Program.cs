82e3b4515bddc8764c22b100804b3ee3820cdf21
Florian Wiesauer <f.wiesauer@htlvb.at>
2025-09-16 11:53:54 +0200

    for added

--- Program.cs
+++ Program.cs
@@ -9,13 +9,18 @@ if (true)
 int a,b;
 a = 2;
 b = 3;
-if (a + b == 10)
-{
-    Console.WriteLine("Math");
-}
-else
-{
-    Console.WriteLine("not Math");
-    a = 8;
+for (int i = 0; i < 12; i++) {
+    if (a + b == 10)
+    {
+        Console.WriteLine("Math");
+        a = 2;
+        b = 3;
+    }
+    else
+    {
+        Console.WriteLine("not Math");
+        a += 1;
+        b += 1;
+    }
 }
 
